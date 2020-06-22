2d7aef311b2b0f75342719232a388e285295172d
Miguel Angel <ing.dev.manzano@gmail.com>
2020-06-22 17:38:54 -0500

    react

--- mifrontend/src/App.js
+++ mifrontend/src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+
+  const [pokemon, setPokemon] = useState("");
+  const [pokemonImagenUrl, setPokemonImagenUrl] = useState("");
+  const [yaLeDioClick, setYaLeDioClick] = useState(false);
+  const [yaAtrapados, setYaAttrapados] = useState([]);
+
+  const darClick = async() =>{
+    setYaLeDioClick(true);
+  }
+
+  const cambioInput = (e) =>{
+    console.log(e.target);
+    setPokemon(e.target.value)
+    
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Pokemon Catcher</h1>
+        <input
+        value={pokemon}
+        onChange={(e) => (cambioInput(e))}
+        /><br/>
+        <button onClick={darClick}>Catch Pokemon</button>
+      </div>
+
     </div>
   );
 }
